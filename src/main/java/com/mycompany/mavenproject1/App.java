9dca6d52d213954244faa799f201a3e5c49d1e1c
miklymchuk <miklymchuk@gmail.com>
2026-09-02 18:11:33 -0400

    Added functionality for the register button. The buttons are displayed in a (not nicely organized yet) grid.

--- src/main/java/com/mycompany/mavenproject1/App.java
+++ src/main/java/com/mycompany/mavenproject1/App.java
@@ -34,26 +34,69 @@ public class App extends Application {
         
         var register = new Button("Register");
         register.setDisable(true);
-        if (firstNameText.getText() != "" && lastNameText.getText() != "" && emailText.getText() != "" && passwordText.getText() != "") {
+        if (firstNameText.getText().isEmpty() && lastNameText.getText().isEmpty() && emailText.getText().isEmpty() && passwordText.getText().isEmpty()) {
             register.setDisable(false);
         }
-        Boolean digit = false;
-        Boolean character = false;
-        String letters = "abcdefghijklmnopqrstuvwxyz";
-        String digits = "0123456789";
-        register.setOnAction(e -> {
-            if ((emailText.getText().contains("@") && emailText.getText().contains(".c"))) {
-            for (int a = 0; a < emailText.getText().length(); a++) {
-                
-            }
-        }
-        });
         var clear = new Button("Clear");
         
         var check = new Label("");
         
+        register.setOnAction(e -> {
+            Boolean emailCheck = false;
+            Boolean digitCheck = false;
+            Boolean characterCheck = false;
+            String letters = "abcdefghijklmnopqrstuvwxyz";
+            String digits = "0123456789";
+            String emailCheck2 = "";
+            if (emailText.getText().contains("@")) {
+                for (int a = 0; a < emailText.getText().length(); a++) {
+                    if (emailText.getText().charAt(a) == '@') {
+                        emailCheck2 = emailText.getText().substring(a, emailText.getText().length());
+                        
+                    }
+                }
+                if (emailCheck2.contains(".com") || emailCheck2.contains(".ca")) {
+                    emailCheck = true;
+                }
+            }
+            
+            for (int b = 0; b < emailText.getText().length() && (digitCheck == false || characterCheck == false); b++) {
+                if (digits.contains(passwordText.getText().substring(b, b + 1))) {
+                    digitCheck = true;
+                }
+                if (letters.contains(passwordText.getText().substring(b, b + 1))) {
+                    characterCheck = true;
+                }
+            }
+            
+            if (emailCheck == true) {
+                System.out.println("email good");
+            }
+            if (digitCheck == true) {
+                System.out.println("digit good");
+            }
+            if (characterCheck == true) {
+                System.out.println("character good");
+            }
+            if (emailCheck == true && digitCheck == true && characterCheck == true) {
+                check.setText("Your account has been registered.");
+            } else {
+                check.setText("Error: Your email or password is invalid. Your account has not been registered.");
+            }
+        });
         
-        
+        root.setCenter(grid);
+        grid.add(firstName, 0, 0);
+        grid.add(lastName, 0, 1);
+        grid.add(email, 0, 2);
+        grid.add(password, 0, 3);
+        grid.add(firstNameText, 1, 0);
+        grid.add(lastNameText, 1, 1);
+        grid.add(emailText, 1, 2);
+        grid.add(passwordText, 1, 3);
+        grid.add(register, 0, 4);
+        grid.add(clear, 1, 4);
+        grid.add(check, 0, 5);
         var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
