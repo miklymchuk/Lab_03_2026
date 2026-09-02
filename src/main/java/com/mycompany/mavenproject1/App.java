af691ab1b08f8270e9338e99b3c6692cee3e7a8d
6312398 <6312398@E50132-3MMJW54.VanierCollege.intra>
2026-09-02 15:20:56 -0400

    Created classes for the required parts of the lab.

--- src/main/java/com/mycompany/mavenproject1/App.java
+++ src/main/java/com/mycompany/mavenproject1/App.java
@@ -2,6 +2,10 @@ package com.mycompany.mavenproject1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -16,6 +20,22 @@ public class App extends Application {
     public void start(Stage stage) {
         var root = new BorderPane();
         var grid = new GridPane();
+        
+        var firstName = new Label("First Name:");
+        var lastName = new Label("Last Name:");
+        var email = new Label("Email:");
+        var password = new Label("Password:");
+        
+        var firstNameText = new TextField();
+        var lastNameText = new TextField();
+        var emailText = new TextField();
+        var passwordText = new PasswordField();
+        
+        var register = new Button("Register");
+        var clear = new Button("Clear");
+        
+        var check = new Label(" ");
+        
         var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
